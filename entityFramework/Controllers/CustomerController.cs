2cf88051d9e991ac327b4cbf80f24db0cadeb01c
Mohammed Aloshaibi <mohammed.aloshaiby@gmail.com>
2024-01-23 17:40:25 +0300

    first commit

---
+++ entityFramework/Controllers/CustomerController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using entityFramework.Models;
+using entityFramework.Data;
+using static entityFramework.Models.Customer;
+namespace entityFramework.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            /*using عشان ما ترجع تعمل حذف للاوبجيكت عند الانتهاء*/
+
+            using (AppDBContext context = new AppDBContext())
+            {
+                var Customer = context.Customer;
+
+                return View(context.Customer.ToList());
+            }
+           
+        }
+
+        private ActionResult View(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ entityFramework/Data/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using entityFramework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace entityFramework.Data
+{
+
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext()
+        {
+            
+        }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+
+    }
+
+
+}
